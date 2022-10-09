c622b52864a252abfec02cf5191008a71debcf08
Shishir Biyyala <shishir.biyyala@invitae.com>
2022-10-09 15:59:06 -0400

    chore: unblock redundancy across machines, by commenting out clojure mode

--- conf/init-clojure.el
+++ conf/init-clojure.el
@@ -2,7 +2,8 @@
 (require 'ob-clojure)
 
 ;; https://orgmode.org/worg/org-contrib/babel/languages/ob-doc-clojure.html, clojure config
-(add-to-list 'load-path "/Users/shishir.biyyala/elisp/org-mode/lisp")
+;; commenting out on 10/09/22, to unblock redundancy across machines
+;; (add-to-list 'load-path "/Users/shishir.biyyala/elisp/org-mode/lisp")
 
 (setq org-babel-clojure-backend 'cider)
 (require 'cider)
